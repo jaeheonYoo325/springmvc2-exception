264c8e68112e6596c9dafae4ad325f025f95684b
jaeheonYoo325 <yoojh0325@gmail.com>
2022-07-16 22:53:48 +0900

    2022.07.16 : 서블릿 예외 처리 - 인터셉터.

--- src/main/java/com/springmvc2/springmvc2exception/WebConfig.java
+++ src/main/java/com/springmvc2/springmvc2exception/WebConfig.java
@@ -1,9 +1,11 @@
 package com.springmvc2.springmvc2exception;
 
 import com.springmvc2.springmvc2exception.filter.LogFilter;
+import com.springmvc2.springmvc2exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -12,7 +14,15 @@ import javax.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**"); //오류 페이지 경로
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
