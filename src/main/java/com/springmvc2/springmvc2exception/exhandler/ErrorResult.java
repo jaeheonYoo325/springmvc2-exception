77e0135c30b4905462da868121b8111f9b35d96b
jaeheonYoo325 <yoojh0325@gmail.com>
2022-07-17 16:38:15 +0900

    2022.07.17 : @ExceptionHandler.

---
+++ src/main/java/com/springmvc2/springmvc2exception/exhandler/ErrorResult.java
@@ -0,0 +1,11 @@
+package com.springmvc2.springmvc2exception.exhandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResult {
+    private String code;
+    private String message;
+}
